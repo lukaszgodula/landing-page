a2fe7f76f09d1f7f00940babf947ba29e65b8dcc
lukaszgodula <lukaszgodula@gmail.com>
2018-05-10 22:25:16 +0200

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { TeamComponent } from './components/team/team.component';
 
@@ -16,7 +17,8 @@ const APP_ID = 'mig-mig-ssr';
     NavBarComponent,
     CarouselComponent,
     AboutUsComponent,
-    TeamComponent
+    TeamComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: APP_ID }),
